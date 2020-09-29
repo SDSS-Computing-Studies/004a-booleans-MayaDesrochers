2207354be5ac3f4b8b84ee19d9fbb41b7d49c5f7
MayaDesrochers <71406875+MayaDesrochers@users.noreply.github.com>
2020-09-28 17:27:31 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -11,3 +11,11 @@
 # Outputs:
 # "the number is even"
 # "the number is odd"
+a=input("Enter a number")
+a=float(a)
+
+if a%2 ==0:
+    print("the number is even")
+
+else:
+    print("the number is odd")
